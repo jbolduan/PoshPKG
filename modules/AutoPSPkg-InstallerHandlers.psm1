7839c4fabbfd00a5449885bc62a59a5116e3325c
Jeff Bolduan <jbolduan@gmail.com>
2017-02-18 14:46:19 -0600

    added expand exe

--- modules/AutoPSPkg-InstallerHandlers.psm1
+++ modules/AutoPSPkg-InstallerHandlers.psm1
@@ -1,25 +1,67 @@
+#Requires -Modules 7Zip4PowerShell
+
 <#
     TODO:
-        [ ] Implement Expand-EXE
+        [x] Implement Expand-EXE
         [ ] Implement Get-EXEProductVersion
 #>
 function Expand-EXE {
+    <#
+    .SYNOPSIS
+        Takes an executible and extracts it.
+    .DESCRIPTION
+        Used for extracting EXE's that need to be extracted prior to handling their installation or determining version information.
+    .EXAMPLE
+        Extract-EXE -Name "Firefox" -Path C:\Path\To\Firefox.exe -TempDestination "C:\Windows\Temp"
+    .PARAMETER Name
+        Is used as the temp folder under the temp destination.
+    .PARAMETER Path
+       Path to the executible which needs to be extracted.
+    .PARAMETER TempDestination
+        Temporary storage folder, by default it it $env:TEMP.
+    #>
+    
     [CmdletBinding()]
     param (
-        
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+
+        [Parameter(Mandatory=$false)]
+        [string]$TempDestination=$env:TEMP
     )
-    
-    begin {
-    }
-    
     process {
-    }
-    
-    end {
+        if(-not (Test-Path -Path $TempDestination)) {
+            throw [System.IO.DirectoryNotFoundException]::new("Cannot find directory $TempDestination")
+        } else {
+            if(-not (Test-Path -Path "$TempDestination\$Name")) {
+                New-Item -Path "$TempDestination\$Name" -ItemType Directory
+            }
+
+            Expand-7Zip -ArchiveFileName $Path -TargetPath "$TempDestination\$Name"
+        }
     }
 }
 
 function Get-EXEProductVersion {
+    <#
+    .SYNOPSIS
+        Short description
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+
     [CmdletBinding()]
     param (
         
