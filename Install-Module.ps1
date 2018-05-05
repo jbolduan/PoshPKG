75c4c12d2a3f1b0ddd21c1ebe8e16142e9bf363c
Jeff Bolduan <jbolduan@gmail.com>
2018-05-04 23:40:48 -0500

    updated installer to include Powershell 6

--- Install-Module.ps1
+++ Install-Module.ps1
@@ -1,5 +1,14 @@
-if(-not (Test-Path -Path "$($env:ProgramFiles)\WindowsPowerShell\Modules\PoshPKG")) {
-	New-Item -Path "$($env:ProgramFiles)\WindowsPowerShell\Modules" -Name PoshPKG -ItemType Directory -Force
+if (Test-Path -Path "$($env:ProgramFiles)\WindowsPowerShell") {
+    if (-not (Test-Path -Path "$($env:ProgramFiles)\WindowsPowerShell\Modules\PoshPKG")) {
+        New-Item -Path "$($env:ProgramFiles)\WindowsPowerShell\Modules" -Name PoshPKG -ItemType Directory -Force
+    }
+}
+
+if (Test-Path -Path "$($env:ProgramFiles)\PowerShell") {
+    if (-not (Test-Path -Path "$($env:ProgramFiles)\PowerShell\Modules\PoshPKG")) {
+        New-Item -Path "$($env:ProgramFiles)\PowerShell\Modules" -Name PoshPKG -ItemType Directory -Force
+    }
 }
 
 Copy-Item -Path .\modules\* -Destination "$($env:ProgramFiles)\WindowsPowerShell\Modules\PoshPKG" -Force -Recurse
+Copy-Item -Path .\modules\* -Destination "$($env:ProgramFiles)\PowerShell\Modules\PoshPKG" -Force -Recurse
